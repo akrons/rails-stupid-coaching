d92affb845c71b4edad2d9da5b2746ddddd858a0
amelia kroner <amelia.rkroner@gmail.com>
2023-01-17 20:23:14 +0100

    code working, still doing front end

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def ask
+    @question = params[:question]
+  end
+
+  def answer
+    @question = params[:question].downcase
+
+    if @question == 'I am going to work'
+      answer = 'Great!'
+    elsif @question[-1] == '?'
+      answer = 'Silly question, get dressed and go to work!'
+    else
+      answer = "I don't care, get dressed and go to work!"
+    end
+
+    @answer = answer
+  end
+end
